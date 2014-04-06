8f1fb0d29b36bbd64653ed26c29bbe5cc069ecc3
Rob Graeber <bgraeber@gmail.com>
2014-04-06 16:48:05 -0700

    Modified config.js to use default azure port

--- config.js
+++ config.js
@@ -6,7 +6,18 @@ exports.env = {
   'apiport'  : pe.FEEDME_WEB_API_PORT,
   'feuri'    : pe.FEEDME_FRONTEND_URI,
   'feuriabs' : pe.FEEDME_FRONTEND_URI+':'+pe.FEEDME_FRONTEND_PORT,
-  'feport'   : pe.FEEDME_FRONTEND_PORT,
+  'feport'   : pe.FEEDME_FRONTEND_PORT || pe.PORT,
   'cookie'   : pe.FEEDME_FRONTEND_COOKIE,
   'session'  : pe.FEEDME_FRONTEND_SESSION
 };
+// exports.env = {
+//   'env'      : pe.FEEDME_ENV,
+//   'apiuri'   : pe.FEEDME_WEB_API_URI,
+//   'apiuriabs': pe.FEEDME_WEB_API_URI +':'+pe.FEEDME_WEB_API_PORT,
+//   'apiport'  : pe.FEEDME_WEB_API_PORT,
+//   'feuri'    : pe.FEEDME_FRONTEND_URI,
+//   'feuriabs' : pe.FEEDME_FRONTEND_URI+':'+pe.FEEDME_FRONTEND_PORT,
+//   'feport'   : pe.FEEDME_FRONTEND_PORT,
+//   'cookie'   : pe.FEEDME_FRONTEND_COOKIE,
+//   'session'  : pe.FEEDME_FRONTEND_SESSION
+// };
