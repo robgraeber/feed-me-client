df1e2cc89025c50806b63df12cb62906b81a5eb6
Fabrice Colas <pub@grano-salis.net>
2014-04-10 23:36:21 -0700

    Add longitudinal offset to the map center

--- public/controllers/index.js
+++ public/controllers/index.js
@@ -5,15 +5,16 @@ app.controller('HomeController',
       "Wed", "Thu", "Fri", "Sat" ] });  
     $scope.tagStyle = 'float:right;margin:0 4px 0 4px;opacity:.5;position:relative;height:17px';
 
-    mapElement       = document.getElementById('map');
-    mapOptions       = { 'center': new google.maps.LatLng(37.7, -122.4), 'zoom': 12, "styles": [{"featureType":"water","stylers":[{"color":"#021019"}]},{"featureType":"landscape","stylers":[{"color":"#08304b"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#0c4152"},{"lightness":5}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#0b434f"},{"lightness":25}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#0b3d51"},{"lightness":16}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"}]},{"elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"elementType":"labels.text.stroke","stylers":[{"color":"#000000"},{"lightness":13}]},{"featureType":"transit","stylers":[{"color":"#146474"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#144b53"},{"lightness":14},{"weight":1.4}]}]};
+    mapOffset         = -0.125;
+    mapElement        = document.getElementById('map');
+    mapOptions        = { 'center': new google.maps.LatLng(37.7, -122.4+mapOffset), 'zoom': 12, "styles": [{"featureType":"water","stylers":[{"color":"#021019"}]},{"featureType":"landscape","stylers":[{"color":"#08304b"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#0c4152"},{"lightness":5}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#0b434f"},{"lightness":25}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#0b3d51"},{"lightness":16}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"}]},{"elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"elementType":"labels.text.stroke","stylers":[{"color":"#000000"},{"lightness":13}]},{"featureType":"transit","stylers":[{"color":"#146474"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#144b53"},{"lightness":14},{"weight":1.4}]}]};
 
     $scope.initMap    = function(){
       var dat         = $scope.events; 
       var addr        = $scope.address || 'San+Francisco';
       return GeocodeService.get(addr).then(function(geoCenter){
         geoloc        = geoCenter.data.results[0].geometry.location;
-        mapOptions['center'] = new google.maps.LatLng(geoloc.lat, geoloc.lng);
+        mapOptions['center'] = new google.maps.LatLng(geoloc.lat, geoloc.lng+mapOffset);
         $scope.map    = new google.maps.Map(document.getElementById('map'), mapOptions);
         for (var i    = 0; i < dat.length ; i++) {
           if(dat[i].marker){
